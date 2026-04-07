812d38d2536e86ab4d97c25370e57487c0707204
DELL <DELL@192.168.1.79>
2026-04-07 16:47:20 +0800

    uploud

---
+++ amarmend/src/Alab2/lab2.java
@@ -0,0 +1,16 @@
+package Alab2;
+
+import java.util.Scanner;
+
+public class lab2 {
+	
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		
+		System.out.print(" radius:");
+		double r = input.nextDouble();
+		double bvslvvrynurt = 2*Math.PI*r;
+		System.out.println("bvslvvryn urt: = " + bvslvvrynurt);
+		input.close();
+	}
+}
